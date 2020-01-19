101a175d54624009b56efe0de1df20ae5802424a
ychiodi <parkietje@wlan-145-94-178-146.wlan.tudelft.nl>
2020-01-19 18:30:36 +0100

    update gitignore, update Readme, add resume, add links to social media

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-** Webpage template**
- Gatsby(https://www.gatsbyjs.org/) starter based on the Dimension site template, designed by HTML5 UP. Check out https://codebushi.com/gatsby-starters/
-
+# Webpage template
+[Gatsby](https://www.gatsbyjs.org/) starter based on the Dimension site template, designed by HTML5 UP. Check out https://codebushi.com/gatsby-starters/#    
 ## Preview
 https://parkietje.github.io/webpage/
--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,9 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li> */}
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Resume</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>Projects</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
